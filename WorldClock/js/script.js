fa8902d6e17502bbfa9a3d03533f0b805932a4a0
David Marlon dos Santos <122294044+David-Santos94@users.noreply.github.com>
2023-03-06 20:14:14 +0000

    em desenvolvimento

--- WorldClock/js/script.js
+++ WorldClock/js/script.js
@@ -12,18 +12,50 @@ setInterval(function time() {
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
-    let di = dateToday.getDay();
-    let me = dateToday.getMonth();
-    let an = dateToday.getFullYear();
+    let d = dateToday.getDate();
+    let m = dateToday.getMonth();
+    let a = dateToday.getFullYear();
 
     if (hr < 10) {hr = '0' + hr;}
     if (min < 10) {min = '0' + min;}
     if (sec < 10) {sec = '0' + sec;}
-
+    if (d < 10) {d = '0' + d;}
+    if (m < 10) {m = '0' + m;}
 
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = sec;
-    
+    dia.textContent = d;
+
+    switch (m){
+        case 0:
+            break;
+        case 1:
+            mes.textContent = Março;
+            break;
+        case 2:
+            var string = 'Março';
+            mes.textContent = string;
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
+    }
+    ano.textContent = a;
 })
 
