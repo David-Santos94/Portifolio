d3d980fec5a1c6e1d21cbb7e2d395852839651a5
David Marlon dos Santos <122294044+David-Santos94@users.noreply.github.com>
2023-03-12 15:47:02 +0000

    versão 1.0
    horas e data com mudança de background (dia, tarde e noite).

--- WorldClock/js/script.js
+++ WorldClock/js/script.js
@@ -20,42 +20,59 @@ setInterval(function time() {
     if (min < 10) {min = '0' + min;}
     if (sec < 10) {sec = '0' + sec;}
     if (d < 10) {d = '0' + d;}
-    if (m < 10) {m = '0' + m;}
 
-    horas.textContent = hr;
-    minutos.textContent = min;
+    horas.textContent = hr + ':';
+    minutos.textContent = min + ':';
     segundos.textContent = sec;
     dia.textContent = d;
 
     switch (m){
         case 0:
+            mes.textContent = 'Janeiro';
             break;
         case 1:
-            mes.textContent = Março;
+            mes.textContent = 'Fevereiro';
             break;
         case 2:
-            var string = 'Março';
-            mes.textContent = string;
+            mes.textContent = 'Março';
             break;
         case 3:
+            mes.textContent = 'Abril';
             break;
         case 4:
+            mes.textContent = 'Maio';
             break;
         case 5:
+            mes.textContent = 'Junho';
             break;
         case 6:
+            mes.textContent = 'Julho';
             break;
         case 7:
+            mes.textContent = 'Agosto';
             break;
         case 8:
+            mes.textContent = 'Setembro';
             break;
         case 9:
+            mes.textContent = 'Outubro';
             break;
         case 10:
+            mes.textContent = 'Novembro';
             break;
         case 11:
+            mes.textContent = 'Dezembro';
             break;
     }
     ano.textContent = a;
+
+    if (hr >= 06) {
+        document.querySelector("body").setAttribute("class","day")
+    }
+    if (hr >= 18) {
+        document.querySelector("body").setAttribute("class","afternoon")
+    }if (hr >= 20 || hr < 06) {
+        document.querySelector("body").setAttribute("class","night")
+    }
 })
 
