619878283e5b04dbe22fa3f5067f0fd6e76cf036
David Marlon dos Santos <122294044+David-Santos94@users.noreply.github.com>
2023-04-08 15:44:44 +0100

    em desenvolvimento

--- Calculator/js/script.js
+++ Calculator/js/script.js
@@ -1,4 +1,23 @@
+
 function insert(num){
     var numero = document.getElementById('tela').innerHTML;
+    atualTela(numero, num);
+}
+
+function allClean(){
+    document.getElementById('tela').innerHTML = "";
+}
+
+function backClean(){
+    let tela = document.getElementById('tela').innerHTML;
+    document.getElementById('tela').innerHTML = tela.substring(0, tela.length -1);
+}
+
+function calcular(){
+
+
+}
+
+function atualTela(numero, num){
     document.getElementById('tela').innerHTML = numero + num;
 }
